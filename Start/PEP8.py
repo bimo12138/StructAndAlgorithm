8341f660bd00144977cf774b07a33d6b29584b20
bimo <1371639183@qq.com>
2020-07-17 16:22:04 +0800

    初始化

---
+++ Start/PEP8.py
@@ -0,0 +1,19 @@
+"""
+@author:      13716
+@date-time:   2020/6/25-23:20
+@ide:         PyCharm
+@name:        PEP8.py
+"""
+
+"""
+    遵守最基本的PEP8规范，是 python coder 的最起码的准则
+    1. 缩进使用四个空格而不是tab【现在很多的IDE都支持这个功能】
+    2. class 首字母 大写 单数状态 【User not Users】
+    3. function 成员函数 使用 下划线命名法
+    4. 大写字母表述数据结构的名称 小写字母表示对象
+    5. 常量使用大写字母
+    6. docstring 在模块、类、函数的第一个语句的前面，通常为三个双引号
+    # NOTE
+    7. __str__ => str()
+    8. 
+"""
---
+++ Start/__init__.py
@@ -0,0 +1,6 @@
+"""
+@author:      13716
+@date-time:   2020/6/25-23:20
+@ide:         PyCharm
+@name:        __init__.py.py
+"""
